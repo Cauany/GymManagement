34dacce457e172bfa126ffda081d2c216c2c8985
Cauany Souza <souza.cauany99@gmail.com>
2020-04-15 10:36:56 -0300

    Configuring routes

--- routes.js
+++ routes.js
@@ -1,16 +1,19 @@
+//All requires
 const express = require('express')
 const routes = express.Router()
 
+//Routes
 routes.get('/', function(req, res){
-    return res.redirect('/instructors')
+    return res.redirect("/instructors")
 })
 
 routes.get('/instructors', function(req, res){
-    return res.render('index')
+    return res.render("instructors/index")
 })
 
 routes.get('/members', function(req, res){
-    return res.send('members')
+    return res.send("members")
 })
 
+//Exports Routes
 module.exports = routes
